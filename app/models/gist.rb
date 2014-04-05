f8226f048c15709179ae3e9f2335b53abe243dc1
Patryk Acewicz <patryk.acewicz@gmail.com>
2014-04-05 22:43:28 +0200

    search improvement

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -1,5 +1,5 @@
 class Gist < ActiveRecord::Base
 	def self.search(search)
-	  find(:all, :conditions => ['lang LIKE ?', "%"+search+"%"])
+	  find(:all, :conditions => ['lang LIKE ? OR description LIKE ? OR snippet LIKE ?', "%"+search+"%", "%"+search+"%", "%"+search+"%"])
 	end
 end
